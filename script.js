cdfdf45123679d4f776291df1b3bf6c95a853b88
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-11 07:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(nums, target) {
+ if (nums.length < 3) {
+     return null; // Not enough elements to form a triplet
+    }
+
+    nums.sort((a, b) => a - b); // Sort the array in ascending order
+
+    let closestSum = nums[0] + nums[1] + nums[2]; // Initialize the closest sum to the sum of the first three elements
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const currentSum = nums[i] + nums[left] + nums[right];
+
+            if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
+	
   
 }
 
